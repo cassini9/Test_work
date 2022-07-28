947b38ac297fc269a3b4f8737cc9415ffd52cfa4
Fedor <fedormaks@gmail.com>
2022-07-28 15:54:49 +0300

    algorithm and code upgraded, final commit

--- Test_work/Program.cs
+++ Test_work/Program.cs
@@ -1,5 +1,4 @@
-﻿
-string[] StringArray = new string[] { "Monday", "Tuesday", "Planet", "Sun", "To", "Sunday", "456" };
+﻿string[] StringArray = new string[] { "Monday", "Tuesday", "Planet", "Sun", "To", "Sunday", "456" };
 
 void ShowArray(string[] array)
 {
@@ -7,7 +6,6 @@ void ShowArray(string[] array)
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
-        //Console.WriteLine();
     }
 
 }
@@ -17,12 +15,16 @@ ShowArray(StringArray);
 string[] ThreeCellElements(string[] array)
 {
     string[] ArrayNew = new string[array.Length];
-
+    int j = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length < 4)
-        ArrayNew[i] = array[i];
+        {
+            ArrayNew[j] = array[i];
+            j++;
+        }
         else continue;
+
     }
     Console.WriteLine();
     return ArrayNew;
