0772d1bf9d01e2436f238d12732def667cac6a9c
Fedor <fedormaks@gmail.com>
2022-07-28 12:59:31 +0300

    added code

--- Test_work/Program.cs
+++ Test_work/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] StringArray = new string[] { "Monday", "Tuesday", "Planet", "Sun", "To", "Sunday", "456" };
+
+void ShowArray(string[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        //Console.WriteLine();
+    }
+
+}
+
+ShowArray(StringArray);
+
+string[] ThreeCellElements(string[] array)
+{
+    string[] ArrayNew = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        ArrayNew[i] = array[i];
+        else continue;
+    }
+    Console.WriteLine();
+    return ArrayNew;
+}
+
+ShowArray(ThreeCellElements(StringArray));
